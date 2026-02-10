ce838dbdd1b839bd60c4c7a1635660c557a1149e
Katharina Cheng <qiaoyc2@uw.edu>
2026-02-09 18:56:25 -0800

    Initial Commit

---
+++ ikidTests/ikidTests.swift
@@ -0,0 +1,17 @@
+//
+//  ikidTests.swift
+//  ikidTests
+//
+//  Created by Katharina Cheng on 2/9/26.
+//
+
+import Testing
+@testable import ikid
+
+struct ikidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
